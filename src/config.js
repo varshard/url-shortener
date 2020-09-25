2b30d13eeabec5af279ce14521cef38df2071389
varshard <varshard0@gmail.com>
2020-09-25 16:02:42 +0700

    bugfix(log): Fix LOG not processed correctly.

--- src/config.js
+++ src/config.js
@@ -3,9 +3,11 @@ if (isNaN(maxUrlLength)) {
 	maxUrlLength = 2000;
 }
 
+const logEnabled = process.env.LOG === 'true';
+
 module.exports = {
 	dbUrl: process.env.DB_URL,
-	log: process.env.LOG,
+	log: logEnabled,
 	host: process.env.HOST_URL,
 	maxUrlLength
 };
